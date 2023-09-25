8dc27fde91c6bba12a2927ef9c6c36d8b6207e44
WheatleyCODE <marmazy8@yandex.ru>
2023-09-25 19:02:01 +0300

    [FEAT] Add tests for className utility

---
+++ src/shared/lib/class-names/classNames.test.ts
@@ -0,0 +1,23 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('1 Main param', () => {
+    expect(classNames('class')).toBe('class');
+  });
+
+  test('2 Params', () => {
+    expect(classNames('class', { disable: true, flex: false }, [])).toBe('class disable');
+  });
+
+  test('3 Params', () => {
+    expect(classNames('class', { disable: true, flex: false }, ['button', 'el'])).toBe('class button el disable');
+  });
+
+  test('3 Params', () => {
+    expect(classNames('class', { disable: true, flex: true }, ['button', 'el'])).toBe('class button el disable flex');
+  });
+
+  test('3 Params', () => {
+    expect(classNames('class', { disable: true, flex: undefined }, ['button', 'el'])).toBe('class button el disable');
+  });
+});
