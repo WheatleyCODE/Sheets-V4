b2a646ccc9df805ae970678447113d50c7df37c5
WheatleyCODE <marmazy8@yandex.ru>
2023-09-25 21:44:53 +0300

    [FEAT] Add BundleAnalyzerPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import BundleAnalyzerPlugin from 'webpack-bundle-analyzer';
 import { type BuildOptions } from './types/config';
 
 export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => {
@@ -17,5 +18,6 @@ export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPlu
       __IS_DEV__: JSON.stringify(isDev),
     }),
     new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin.BundleAnalyzerPlugin({ openAnalyzer: false }),
   ];
 };
