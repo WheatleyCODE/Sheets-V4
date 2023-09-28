4afd18c071d68e20a8aefece9c24fd6deab7f471
WheatleyCODE <marmazy8@yandex.ru>
2023-09-28 17:37:08 +0300

    [FIX] Restore imports. edit yml config, test actions build run

--- src/shared/lib/iterators/index.ts
+++ src/shared/lib/iterators/index.ts
@@ -1,5 +1,5 @@
 export { intoAsyncIterator } from './into-async-iterator/IntoAsyncIterator';
 export { intoAsyncIter, AsyncIter } from './async-iter/asyncIter';
 export { intoIterator } from './into-iterator/intoIterator';
-export { intoIter, Iter } from './sync-iter/Iter';
+export { intoIter, Iter } from './iter/Iter';
 export { on, onStream } from './on/on';
