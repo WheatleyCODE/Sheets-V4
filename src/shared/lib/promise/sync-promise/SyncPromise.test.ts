d06cd5dec30e63054e3d67e79758a3c3ba86d716
WheatleyCODE <marmazy8@yandex.ru>
2023-10-10 19:34:59 +0300

    [FEAT] Add SyncPromise

---
+++ src/shared/lib/promise/sync-promise/SyncPromise.test.ts
@@ -0,0 +1,62 @@
+import { SyncPromise } from './SyncPromise';
+
+describe('SyncPromise', () => {
+  test('SyncPromise works', () => {
+    const promise = new SyncPromise<number>((resolve) => resolve(42));
+
+    let finallyIsCall = false;
+    let catchIsCall = false;
+    let result = 0;
+
+    promise
+      .then((value) => value + 100)
+      .then((value) => value + 10)
+      .then((value) => {
+        result = value - 2;
+        return value - 2;
+      })
+      .catch(() => {
+        catchIsCall = true;
+      })
+      .finally(() => {
+        finallyIsCall = true;
+      });
+
+    expect(promise[Symbol.toStringTag]).toBe('SyncPromise');
+    expect(result).toBe(150);
+    expect(catchIsCall).toBe(false);
+    expect(finallyIsCall).toBe(true);
+  });
+
+  test('SyncPromise error', () => {
+    const promise = new SyncPromise<number>((resolve) => resolve(42));
+
+    let finallyIsCall = false;
+    let catchIsCall = false;
+    let result = 0;
+
+    promise
+      .then((value) => value + 100)
+      .then((value) => value + 10)
+      .then((value) => {
+        result = value - 2;
+        return value - 2;
+      })
+      .then(() => {
+        // @ts-ignore
+        document.blabla();
+        result = 0;
+      })
+      .catch(() => {
+        catchIsCall = true;
+      })
+      .finally(() => {
+        finallyIsCall = true;
+      });
+
+    expect(promise[Symbol.toStringTag]).toBe('SyncPromise');
+    expect(result).toBe(150);
+    expect(catchIsCall).toBe(true);
+    expect(finallyIsCall).toBe(true);
+  });
+});
---
+++ src/shared/lib/promise/sync-promise/SyncPromise.ts
@@ -0,0 +1,82 @@
+export class SyncPromise<T> implements Promise<T> {
+  #value?: T;
+  #reason?: any;
+  #state: 'pending' | 'fulfilled' | 'rejected' = 'pending';
+  [Symbol.toStringTag] = 'SyncPromise';
+
+  constructor(executor: (resolve: (value?: T) => void, reject: (reason?: any) => void) => void) {
+    try {
+      executor(this.#resolve.bind(this), this.#reject.bind(this));
+    } catch (error) {
+      this.#reject(error);
+    }
+  }
+
+  #resolve(value?: T) {
+    if (this.#state === 'pending') {
+      this.#state = 'fulfilled';
+      this.#value = value;
+    }
+  }
+
+  #reject(reason?: any) {
+    if (this.#state === 'pending') {
+      this.#state = 'rejected';
+      this.#reason = reason;
+    }
+  }
+
+  then<TResult1 = T, TResult2 = never>(
+    onFulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | null | undefined,
+    onRejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null | undefined,
+  ): Promise<TResult1 | TResult2> {
+    return new SyncPromise<TResult1 | TResult2>((resolve, reject) => {
+      if (this.#state === 'fulfilled' && onFulfilled) {
+        try {
+          const result = onFulfilled(this.#value as T);
+          resolve(result as any);
+        } catch (error) {
+          reject(error);
+        }
+      } else if (this.#state === 'rejected' && onRejected) {
+        try {
+          const result = onRejected(this.#reason);
+          resolve(result as any);
+        } catch (error) {
+          reject(error);
+        }
+      } else {
+        resolve();
+      }
+    });
+  }
+
+  catch<TResult = never>(
+    onRejected?: ((reason: any) => TResult | PromiseLike<TResult>) | null | undefined,
+  ): Promise<T | TResult> {
+    return this.then(undefined, onRejected);
+  }
+
+  finally(onFinally: () => void): Promise<T> {
+    return this.then(
+      (value) => {
+        onFinally();
+        return value;
+      },
+      (reason) => {
+        onFinally();
+        throw reason;
+      },
+    );
+  }
+
+  static resolve<T>(value?: T): Promise<T> {
+    const promise = new SyncPromise<T>((resolve) => resolve(value));
+    return promise;
+  }
+
+  static reject<T = never>(reason?: any): Promise<T> {
+    const promise = new SyncPromise<T>((_, reject) => reject(reason));
+    return promise;
+  }
+}
