a74cf38145a4cc21f51c3c1b3a66b55ff299905c
WheatleyCODE <marmazy8@yandex.ru>
2023-11-10 15:54:54 +0300

    [FEAT] Add vite

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react(), svgr({ svgrOptions: { exportType: 'default' } })],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+  define: {
+    __IS_DEV__: JSON.stringify(true),
+    __PROJECT__: JSON.stringify('frontend'),
+  },
+});
