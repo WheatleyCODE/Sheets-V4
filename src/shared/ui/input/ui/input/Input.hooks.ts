24441e68e76b93deb7014bd6535ef7ffda2967bb
WheatleyCODE <marmazy8@yandex.ru>
2023-12-21 22:10:54 +0300

    [FEAT] Change hook. start refactoring

--- src/shared/ui/input/ui/input/Input.hooks.ts
+++ src/shared/ui/input/ui/input/Input.hooks.ts
@@ -1,37 +1,141 @@
-import { ChangeEvent, useCallback, useEffect, useState } from 'react';
-import type {
-  IUseValidInputParams,
-  IUseValidInputResult,
-  PropsWithUseValidInput,
-  PropsWithoutUseValidInput,
-  ResultWithoutUseValidInput,
-} from './Input.interface';
-import { extractUseDefaultEventsProps, useDefaultEvents } from '@/shared/lib/hooks';
+// import { ChangeEvent, useCallback, useEffect, useState } from 'react';
+// import type {
+//   IUseValidInputParams,
+//   IUseValidInputResult,
+//   PropsWithUseValidInput,
+//   PropsWithoutUseValidInput,
+//   ResultWithoutUseValidInput,
+// } from './Input.interface';
+// import { extractUseDefaultEventsProps, useDefaultEvents } from '@/shared/lib/hooks';
 
-export const extractUseValidInputProps = <P extends object, EL, T extends string>(
-  props: PropsWithUseValidInput<P, EL, T>,
-): ResultWithoutUseValidInput<P, EL, T> => {
-  const [props1, defData, defHandlers] = extractUseDefaultEventsProps(props);
+import { Cache } from '@/shared/lib/cache';
+import { HookBuilder } from '@/shared/lib/hook-builder';
+import {
+  IUseChangeParams,
+  IUseChangeResult,
+  IUseClickParams,
+  IUseClickResult,
+  IUseFocusParams,
+  IUseFocusResult,
+  useChange,
+  useClick,
+  useFocus,
+} from '@/shared/lib/hooks/hooks-for-builder';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
-  const { value, changeValue, isError, changeIsError, validError, changeValidError, onChange, ...anotherProps } =
-    props1;
+// export const extractUseValidInputProps = <P extends object, EL, T extends string>(
+//   props: PropsWithUseValidInput<P, EL, T>,
+// ): ResultWithoutUseValidInput<P, EL, T> => {
+//   const [props1, defData, defHandlers] = extractUseDefaultEventsProps(props);
 
-  return [
-    anotherProps as PropsWithoutUseValidInput<P, EL, T>,
-    { ...defData, value, changeValue, isError, changeIsError, validError, changeValidError },
-    { ...defHandlers, onChange },
-  ];
+//   const { value, changeValue, isError, changeIsError, validError, changeValidError, onChange, ...anotherProps } =
+//     props1;
+
+//   return [
+//     anotherProps as PropsWithoutUseValidInput<P, EL, T>,
+//     { ...defData, value, changeValue, isError, changeIsError, validError, changeValidError },
+//     { ...defHandlers, onChange },
+//   ];
+// };
+
+// export const useValidInput = <T = string>(
+//   params: IUseValidInputParams<T> = {},
+// ): IUseValidInputResult<T, HTMLInputElement> => {
+//   const { input = { initialValue: '' as T, validators: [] }, default: def } = params;
+
+//   const { data, handlers } = useDefaultEvents<HTMLInputElement>(def);
+//   const [value, setValue] = useState(input.initialValue);
+//   const [validError, setValidError] = useState<null | string>(null);
+//   const [isError, setIsError] = useState(false);
+
+//   useEffect(() => {
+//     setIsError(!!(data.isTouched && validError));
+//   }, [data.isTouched, validError]);
+
+//   const changeValidError = useCallback((value: string | null) => {
+//     setValidError(value);
+//   }, []);
+
+//   const changeIsError = useCallback((boolean: boolean) => {
+//     setIsError(boolean);
+//   }, []);
+
+//   const changeValue = useCallback(
+//     (string: T) => {
+//       setValue(string);
+
+//       if (typeof string === 'string') {
+//         input.validators?.forEach((fn) => {
+//           setValidError(fn(string));
+//         });
+//       }
+//     },
+//     [input.validators],
+//   );
+
+//   const onChange = useCallback(
+//     (e: ChangeEvent<HTMLInputElement>) => {
+//       setValue(e.target.value as T);
+
+//       input.validators?.forEach((fn) => {
+//         setValidError(fn(e.target.value));
+//       });
+//     },
+//     [input.validators],
+//   );
+
+//   return {
+//     data: {
+//       ...data,
+//       value,
+//       changeValue,
+//       isError,
+//       changeIsError,
+//       validError,
+//       changeValidError,
+//     },
+
+//     handlers: {
+//       ...handlers,
+//       onChange,
+//     },
+//   };
+// };
+
+export type UseValidInputMergedTypes<T, V extends string> = [
+  IUseChangeResult<T, V>,
+  IUseFocusResult<T>,
+  IUseClickResult<T>,
+];
+
+const useValidInputEvents = new HookBuilder<UseValidInputMergedTypes<HTMLInputElement, string>, HTMLInputElement>()
+  .enableMemo(new Cache())
+  .addHook(useChange)
+  .addHook(useFocus)
+  .addHook(useClick)
+  .build();
+
+export type UseValidInputResult = ReturnType<typeof useValidInput>;
+
+export type UseValidInputParams<T extends HTMLElement> = {
+  useValidInput?: {
+    useChange?: IUseChangeParams<T, string>;
+    useFocus?: IUseFocusParams<T>;
+    useClick?: IUseClickParams<T>;
+  };
 };
 
-export const useValidInput = <T = string>(
-  params: IUseValidInputParams<T> = {},
-): IUseValidInputResult<T, HTMLInputElement> => {
-  const { input = { initialValue: '' as T, validators: [] }, default: def } = params;
+export const useValidInput = (params: UseValidInputParams<HTMLInputElement> = {}) => {
+  const { useValidInput } = params;
 
-  const { data, handlers } = useDefaultEvents<HTMLInputElement>(def);
-  const [value, setValue] = useState(input.initialValue);
   const [validError, setValidError] = useState<null | string>(null);
   const [isError, setIsError] = useState(false);
+
+  const { data, dataChangers, eventHandlers, ref } = useValidInputEvents({
+    useChange: useValidInput?.useChange,
+    useFocus: useValidInput?.useFocus,
+    useClick: useValidInput?.useClick,
+  });
 
   useEffect(() => {
     setIsError(!!(data.isTouched && validError));
@@ -45,44 +149,15 @@ export const useValidInput = <T = string>(
     setIsError(boolean);
   }, []);
 
-  const changeValue = useCallback(
-    (string: T) => {
-      setValue(string);
-
-      if (typeof string === 'string') {
-        input.validators?.forEach((fn) => {
-          setValidError(fn(string));
-        });
-      }
-    },
-    [input.validators],
-  );
-
-  const onChange = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      setValue(e.target.value as T);
-
-      input.validators?.forEach((fn) => {
-        setValidError(fn(e.target.value));
-      });
-    },
-    [input.validators],
-  );
-
   return {
-    data: {
-      ...data,
-      value,
-      changeValue,
-      isError,
-      changeIsError,
-      validError,
-      changeValidError,
-    },
+    data: useMemo(() => ({ ...data, validError, isError }), [data, isError, validError]),
 
-    handlers: {
-      ...handlers,
-      onChange,
-    },
+    dataChangers: useMemo(
+      () => ({ ...dataChangers, changeValidError, changeIsError }),
+      [changeIsError, changeValidError, dataChangers],
+    ),
+
+    eventHandlers,
+    ref,
   };
 };
