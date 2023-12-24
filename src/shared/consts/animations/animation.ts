00da8879293c52411cf6feb8de2727ee667491cf
WheatleyCODE <marmazy8@yandex.ru>
2023-12-24 17:54:52 +0300

    [FEAT] Add animation consts

--- src/shared/consts/animations/animation.ts
+++ src/shared/consts/animations/animation.ts
@@ -3,3 +3,6 @@ export const ANIMATION_DURATION_MS = 100;
 
 export const ANIMATION_SPRING = 0.2;
 export const ANIMATION_SPRING_DELAY = 0.3;
+
+export const ANIMATION_INPUT = 0.3;
+export const ANIMATION_INPUT_DELAY = 0.4;
