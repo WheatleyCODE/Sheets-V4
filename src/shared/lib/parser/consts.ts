5b3bcf9df444ad558741cdaeee455b877904f83c
Дмитрий Бажаев <marmazy8@yandex.ru>
2024-01-24 16:59:47 +0300

    [FEAT] Add token types

--- src/shared/lib/parser/consts.ts
+++ src/shared/lib/parser/consts.ts
@@ -1,11 +1,34 @@
 export enum TokenTypes {
+  // Factories
   TAG = 'TAG',
   TAKE = 'TAKE',
+
+  // Combinators
   CHECK_NEXT = 'CHECK_NEXT',
   SEQ = 'SEQ',
   OR = 'OR',
   REPEAT = 'REPEAT',
   OPT = 'OPT',
+
+  // Math
+  SIGN = 'SIGN',
+  INT = 'INT',
+
+  // Number
+  NUMBER = 'NUMBER',
+  NUMBER_SIGN = 'NUMBER_SIGN',
+  NUMBER_INT = 'NUMBER_INT',
+  NUMBER_FRACTION = 'NUMBER_FRACTION',
+  NUMBER_EXPONENT = 'NUMBER_EXPONENT',
+
+  // Exponent
+  EXPONENT = 'EXPONENT',
+  EXPONENT_SIGN = 'EXPONENT_SIGN',
+  EXPONENT_INT = 'EXPONENT_INT',
+
+  // Symbols
+  DOT = 'DOT',
+  EXPONENT_SYMBOL = 'EXPONENT_SYMBOL',
 }
 
 export enum ParserStates {
